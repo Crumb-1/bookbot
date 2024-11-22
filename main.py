3822ace81c7d61a8870d8edf506476a8f1a45261
Crumb-1 <crumby.andrew@gmail.com>
2024-11-21 22:36:45 -0500

    added main function and count function

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-with open("books/frankenstein.txt") as f:
-    file_contents = f.read()
-print (file_contents)
+def read_book():
+    with open("/Users/rewc/workspace/github.com/Crumb-1/bookbot/books/frankenstein.txt") as f:
+        file_contents = f.read()
+    return file_contents
+
+def count_words(file_contents):
+    count = 0
+    words = file_contents.split()
+    for i in words:
+        count += 1
+    print (count)
+
+def main():
+
+    file_contents = read_book()
+    count_words(file_contents)
+
+if __name__ == "__main__":
+    main()
