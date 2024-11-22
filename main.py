36f416b4059eb9877530d79aad450e828ee1fe1e
Crumb-1 <crumby.andrew@gmail.com>
2024-11-22 14:50:36 -0500

    add funtion to count charachters in book

--- main.py
+++ main.py
@@ -9,11 +9,24 @@ def count_words(file_contents):
     for i in words:
         count += 1
     print (count)
+def count_charachters(file_contents):
+    lowered_string = file_contents.lower()
+    character_list = [x for x in lowered_string]
+    count_dict ={}
+
+    for i in character_list:
+        if i in count_dict:
+            count_dict[i] += 1
+        else:
+            count_dict[i] = 1
+    
+    print (count_dict)
 
 def main():
 
     file_contents = read_book()
     count_words(file_contents)
+    count_charachters(file_contents)
 
 if __name__ == "__main__":
     main()
